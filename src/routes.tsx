0111a27b1a07cf22aadce945f3d90d417ab1f8ce
Lucas Becker <lucasgabrielbec@gmail.com>
2021-02-11 11:19:30 -0300

    Update routes.tsx
    fixed camel case Component import

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,7 @@ import {
   Link
 } from "react-router-dom";
 import Login from "./pages/login"
-import Landing from "./pages/Landing"
+import Landing from "./pages/landing"
 
 export default function Routes() {
   return (
