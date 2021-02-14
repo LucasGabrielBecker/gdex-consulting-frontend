e23c031b15bc6eac813ea8c5e7f51dcd42f4f72a
Lucas Lopes <lucasgabrielbec@gmail.com>
2021-02-14 12:11:00 -0300

    added serverless api with ts

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { NowRequest, NowResponse } from '@vercel/node'
+
+export default function handler(request: NowRequest, response: NowResponse) {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.json({ name: 'John Doe' })
